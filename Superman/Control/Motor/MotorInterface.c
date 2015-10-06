8e70b07a18e0c344d8cfaf220c6945574df05b1d
monibu1548 <monibu1548@gmail.com>
2015-10-06 14:47:07 +0000

    Control : Motor : Add Motor Interface
    ./Control/Motor/MotorInterface.h
    
    + 모터 동작에 공통적인 부분
    + 초기화 init()
    + 핀 설정 setPin()
    + 속도설정 setSpeed()
    + 정지 stop() 으로 구성되어있음.
    
    + Motor 클래스이며 앞으로 추가되는 모든 장치는 이 클래스를 include 하여 사용할 것.
    + Motor.h 에 의해 indef 로 한 가지 장치만 선택되도록 구성.
    
    ./Control/Motor/MotorInterface.c
    
    + MoterInterface.h 의 구현부
    + 공통부분과 각 장치 특성에 달리 구현되어야 하는부분 분리
    + 장치 구현시 이 파일을 복사하여 해당 장치에 맞게 구현
    + 등록된 장치는 Moter.h에 등록하여 사용

---
+++ Superman/Control/Motor/MotorInterface.c
@@ -0,0 +1,82 @@
+/*
+ * MoterInterface.h
+ * @author  Jingyu Jung
+ * @date    2015.10.07
+ * 
+ * MotorInterface 의 구현부로 동일한 인터페이스를 제공하기 위함.
+ * Motor.h 에 의해 사용되는 장치가 결정되며 해당 파일을 인터페이스로 사용.
+ * 새로운 장치를 추가하기 위해 이 파일을 복사하여 사용.
+ */
+
+
+
+#include "MoterInterface.h"
+
+/*
+ * Motor()
+ * @function : Dafault consturctor
+ */
+Moter::Motor()
+{
+}
+
+
+ /*
+  * init()
+  * @function : set up wiringPi enviroment
+  * @return : -1 Error , 1 OK
+  * @detail : 공통부분. wiringPi 설정 및 속도 초기화
+  */
+int Moter::init()
+{
+    speed = 0;
+    
+}
+
+
+/*
+ * setPin()
+ * @function : Selecting Pin number using softpwm output
+ * @param    : pinNum, pin using pwm gpio
+ * @return   : -1 Error, 1 OK
+ * @detail   : 공통부분. 모터 사용 핀넘버 설정.
+ */
+int Motor::setPin(int pinNum)
+{
+
+}
+
+
+/*
+ * calibration()
+ * @function : Calbration for init setting ESC motor.
+ * @detail   : 각 ESC 의 데이터시트 참조하여 Clibrate 코드 작성
+ */
+void Motor::calibration()
+{
+
+}
+
+
+/*
+ * setSpeed()
+ * @function  : Set speed 
+ * @ param    : Speed value, range ( 0 ~ 2500 )
+ * @ detail   : 공통부분. Range 범위 내의 속도 지정
+ */
+void Moter::setSpeed(int value)
+{
+
+}
+
+
+
+/*
+ * stop()
+ * @function : Stop drone
+ * @detail   : 공통부분. 속도 0 지정 
+ */
+void Moter::stop()
+{
+
+} 
---
+++ Superman/Control/Motor/MotorInterface.h
@@ -0,0 +1,59 @@
+/*
+ * MotorInterface
+ * @author Jingyu Jung
+ * @date 2015.10.07
+ *
+ * 모터 장치를 위한 기본 클래스 파일. 공통된 인터페이스를 제공하기 위해 사용.
+ * 새로운 장치 추가시 이 파일을 include 하여 그대로 사용하도록 함. 
+ * 각 함수들은 새로 추가하는 장치 환경에 따라 설정.
+ */
+class Motor {
+
+    private :
+        int speed;
+        int pin;
+
+    public :
+
+        /*
+         * Motor()
+         * @function : Dafault consturctor
+         */
+        Motor();
+        
+        /*
+         * init()
+         * @function : set up wiringPi enviroment
+         * @return : -1 Error , 1 OK
+         */
+        int init();
+
+        /*
+         * setPin()
+         * @function : Selecting Pin number using softpwm output
+         * @param    : pinNum, pin using pwm gpio
+         * @return   : -1 Error, 1 OK
+         */
+        int setPin(int pinNum);
+
+        /*
+         * calibration()
+         * @function : Calbration for init setting ESC motor. 
+         */
+        void calibration();
+        
+        /*
+         * setSpeed()
+         * @function : 
+         * @ param    : Speed value, range ( 0 ~ 2500 )
+         */
+        void setSpeed(int value);
+
+        /*
+         * stop()
+         * @function : Stop drone
+         */
+        void stop()
+       
+
+}
